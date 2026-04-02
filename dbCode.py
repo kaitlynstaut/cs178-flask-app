0dfd62631f8fd92974f6103e9bdc478b8308d00b
kaitlynstaut <kaitlyn.staut@drake.edu>
2026-04-02 12:03:34 -0500

    update dbCode

--- dbCode.py
+++ dbCode.py
@@ -3,6 +3,7 @@
 # Helper functions for database connection and queries
 
 import pymysql
+import boto3
 import creds
 
 def get_conn():
@@ -17,8 +18,10 @@ def get_conn():
 
 def execute_query(query, args=()):
     """Executes a SELECT query and returns all rows as dictionaries."""
+    conn = get_conn()
     cur = get_conn().cursor(pymysql.cursors.DictCursor)
     cur.execute(query, args)
+    conn.commit()
     rows = cur.fetchall()
     cur.close()
     return rows
